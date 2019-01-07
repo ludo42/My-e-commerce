546c408dcbc820a1bfcd28e45771183e81d5f90c
Elineda <henecro@yahoo.fr>
2019-01-07 11:25:56 +0100

    test

---
+++ test.php
@@ -0,0 +1,24 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Design Tech Académie</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <header>
+    </header>
+    <main>
+
+
+</main>
+    <footer>
+
+    </footer>
+
+
+
+<script src="script.js" type="text/javascript"></script>
+  </body>
+</html>
