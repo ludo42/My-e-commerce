86edeebfbe2345c8d42001bd76b4467d9075458e
Ludo <ludo_42800@hotmail.fr>
2019-01-07 09:50:45 +0100

    lancement

---
+++ main.js
@@ -0,0 +1,19 @@
+// $('.flip').hover(function(){
+//         $(this).find('.card').toggleClass('flipped');
+
+//  });
+
+// onClick=document.getElementsByClasseName('img-thumbnail').style.width='500px';
+
+// let img=document.getElementByID("imgthumbnail")
+
+// img.onclick = function() {
+//     let img=document.getElementByID("imgthumbnail");
+//     console.log(img);
+// };
+
+/*
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
+*/
